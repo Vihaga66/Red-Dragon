a65e54fd8539f39b492919a01c139e097b91199b
Vihaga66 <109607940+Vihaga66@users.noreply.github.com>
2022-08-28 14:07:51 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -58,7 +58,7 @@ global.dragonchat = "true"  // chat bot  "true" / "false" (200 auto replies in t
 global.callblock = "true" // call block "true" / "false" ("true"  to block callers)
 global.antibad = "false" // To remove bad word senders
 global.antispam = "true" // To remove or block spammers
-global.antilink = "true" // To remove group link sharing peoples
+global.antilink = "false" // To remove group link sharing peoples
 global.upsongvoice = "false" // true/false \\ bot can upload song as voice note (it use your data and storage bacause auto download) you can put "false" (anyone can't get songs as voice note / your data is not wasted)
 global.welcom_msg = "true" // Welcome / Goodbye   [true/false]
 
